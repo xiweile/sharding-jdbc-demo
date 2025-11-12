567391d665fb68eca22b7ff80055ea6c8adfe764
weiller0527 <>
2025-11-13 01:48:29 +0800

    集群模式验证

--- src/main/java/com/example/shardingjdbc/ShardingJdbcApplication.java
+++ src/main/java/com/example/shardingjdbc/ShardingJdbcApplication.java
@@ -3,8 +3,9 @@ package com.example.shardingjdbc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.transaction.jta.JtaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = JtaAutoConfiguration.class)
 public class ShardingJdbcApplication {
 
     public static void main(String[] args) {
